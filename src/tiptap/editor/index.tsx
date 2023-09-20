6e8dad074217dbb643843a1883128db00d51a76d
AlbertMok <239856559@qq.com>
2023-09-20 08:38:39 +0800

    update

--- src/tiptap/editor/index.tsx
+++ src/tiptap/editor/index.tsx
@@ -204,9 +204,6 @@ export default function Editor({
         <div className="character-count">{editor?.storage.characterCount.characters()} 个字</div>
         <EditorContent editor={editor} />
       </div>
-      <div>
-        <pre>{JSON.stringify(editor?.getJSON(), null, 2)}</pre>
-      </div>
     </div>
   );
 }
